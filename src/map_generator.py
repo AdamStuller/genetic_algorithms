83b06ad07863b66c1ba31e4669a8b5e74fb8152f
Adam Stuller <adam.stuller@protonmail.com>
2019-03-21 14:17:09 +0100

    reading map functionality added

--- src/map_generator.py
+++ src/map_generator.py
@@ -1,4 +1,5 @@
 from random import randint
+from os import path
 
 
 def generate_map(x, y):
@@ -18,4 +19,32 @@ def generate_map(x, y):
         print()
 
 
-generate_map(50 , 50)
+def read_map():
+    m = []
+    p = path.join('.', 'src', 'maps', 'map1.txt')
+    p_counter = 0
+    with open(p) as fp:
+        for line in fp:
+            line_arr = []
+            for ch in line:
+                if ch != '\n':
+                    line_arr.append(ch)
+                if ch == 'P':
+                    p_counter += 1
+            m.append(line_arr)
+
+    for i in range(0 , len(m)):
+        for j in range(0 , len(m[i])):
+            if m[i][j] == 'S':
+                x, y = i, j
+
+    map_object = {
+        'Treasure_count': p_counter,
+        'Map': m,
+        'Start': (x, y)
+    }
+    return map_object
+
+
+# generate_map(50 , 50)
+print(read_map())
